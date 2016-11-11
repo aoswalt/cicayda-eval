e5b47c3ee330638811c273b9391fda73cf7674e0
Adam Oswalt <email@adamoswalt.com>
2016-11-10 21:54:03 -0600

    framing function

--- framing.js
+++ framing.js
@@ -9,9 +9,21 @@
 //   * frame *
 //   *********
 
-const frame = (stringList) => {
-  const longest = stringList.reduce((prev, cur) => (prev || '').length > cur.length ? prev : cur)
-  const longestLength = longest.length
+const frame = (stringList, frameChar) => {
+  frameChar = frameChar || '*'
+
+  const longestString = stringList.reduce((prev, cur) => (prev || '').length > cur.length ? prev : cur)
+  const longestLength = longestString.length
+
+  const cap = frameChar.repeat(2) + frameChar.repeat(longestLength) + frameChar.repeat(2)
+  const bodyList = stringList.map(str => frameChar + ' ' + str + ' '.repeat(longestLength - str.length) + ' ' + frameChar)
+
+  const resultList = [cap].concat(bodyList).concat([cap])
+  return resultList.join('\n')
 }
 
-console.log('frame', frame(['Hello', 'World', 'in', 'a', 'frame']))
+console.log(frame(['Hello', 'World', 'in', 'a', 'frame']))
+console.log('\n')
+console.log(frame(['A', 'different', 'set', 'of', 'words', 'to', 'frame']))
+console.log('\n')
+console.log(frame(['Another', 'set', 'of', 'words', 'with', 'a', 'different', 'frame'], '#'))
