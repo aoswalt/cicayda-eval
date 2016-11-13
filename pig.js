75d42f0568098ada1ea5a4f666cfecf9551cca01
Adam Oswalt <email@adamoswalt.com>
2016-11-13 15:17:23 -0600

    commenting

--- pig.js
+++ pig.js
@@ -2,6 +2,7 @@
 //English is translated to Pig Latin by taking the first letter of every word, moving it to the end of the word and adding ‘ay’.
 //“The quick brown fox” becomes “Hetay uickqay rownbay oxfay”.
 
+//NOTE(adam): change each word in a string with the vunction
 const changeWords = (string, changeFunc) => {
   const stringList = string.toLowerCase().split(' ')
   const changedList = stringList.map(changeFunc)
@@ -9,8 +10,13 @@ const changeWords = (string, changeFunc) => {
   return changedString.slice(0, 1).toUpperCase() + changedString.slice(1)
 }
 
-const toPigLatin = (string) => changeWords(string, (str) => str.slice(1) + str.slice(0, 1) + 'ay')
-const fromPigLatin = (string) => changeWords(string, (pig) => pig.slice(-3, -2) + pig.slice(0, -3))
+//NOTE(adam): move first character to end and append 'ay'
+const toPigLatin = (string) =>
+  changeWords(string, (str) => str.slice(1) + str.slice(0, 1) + 'ay')
+
+//NOTE(adam): strip ending 'ay' and move last character to beginning
+const fromPigLatin = (string) =>
+  changeWords(string, (pig) => pig.slice(-3, -2) + pig.slice(0, -3))
 
 console.log('The quick brown fox -->', toPigLatin('The quick brown fox'))
 console.log('Hetay uickqay rownbay oxfay -->', fromPigLatin('Hetay uickqay rownbay oxfay'))
