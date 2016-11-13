da890a662b636f260d01c923e1953d8a5f4510d6
Adam Oswalt <email@adamoswalt.com>
2016-11-13 15:31:59 -0600

    commenting

--- framing.js
+++ framing.js
@@ -9,13 +9,19 @@
 //   * frame *
 //   *********
 
+//NOTE(adam): construct a framed list of strings with the given character or '*' as default
 const frame = (stringList, frameChar) => {
+  //NOTE(adam): default '*' as frame character
   frameChar = frameChar || '*'
 
+  //NOTE(adam): get the length of the longest string
   const longestString = stringList.reduce((prev, cur) => (prev || '').length > cur.length ? prev : cur)
   const longestLength = longestString.length
 
+  //NOTE(adam): build the top and bottom cap strings
   const cap = frameChar.repeat(2) + frameChar.repeat(longestLength) + frameChar.repeat(2)
+
+  //NOTE(adam): add frame and space padding to each string
   const bodyList = stringList.map(str => frameChar + ' ' + str + ' '.repeat(longestLength - str.length) + ' ' + frameChar)
 
   const resultList = [cap].concat(bodyList).concat([cap])
