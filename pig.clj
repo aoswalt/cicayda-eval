0eb9cadc2fffc46fa01f874b6900528f2a2e79b0
Adam Oswalt <email@adamoswalt.com>
2016-11-14 13:35:30 -0600

    setup for pig latin in clojure

---
+++ pig.clj
@@ -0,0 +1,12 @@
+;NOTE(adam): a function that translates a text to Pig Latin and back
+
+(defn toPigLatin [string] "toPig")
+(defn fromPigLatin [string] "fromPig")
+
+
+(print "The quick brown fox -->  ")
+(print (toPigLatin "The quick brown fox"))
+(print \newline)
+(print "Hetay uickqay rownbay oxfay -->  ")
+(print (fromPigLatin "Hetay uickqay rownbay oxfay"))
+(print \newline)
