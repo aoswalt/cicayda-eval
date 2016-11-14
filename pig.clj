97a2f2d698f129ce1a7c86c80f6e9e4dc931b31d
Adam Oswalt <email@adamoswalt.com>
2016-11-14 14:08:34 -0600

    apply function to each word and rejoin to single string

--- pig.clj
+++ pig.clj
@@ -1,10 +1,13 @@
 ;NOTE(adam): a function that translates a text to Pig Latin and back
 
 (defn changeWords [string changer]
-  (changer string))
+  (def stringList (clojure.string/split string #" "))
+  (def changedList (map changer stringList))
+  (def changedString (reduce #(str %1 " " %2) changedList))
+  changedString)
 
-(defn toPigLatin [string] (changeWords string #(str "toPig " %1)))
-(defn fromPigLatin [string] (changeWords string #(str "fromPig " %1)))
+(defn toPigLatin [string] (changeWords string #(str "toPig" %1)))
+(defn fromPigLatin [string] (changeWords string #(str "fromPig" %1)))
 
 
 (print "The quick brown fox -->  ")
