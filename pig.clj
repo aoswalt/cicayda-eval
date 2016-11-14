d5c48e38ba1a7b7d3c3ee8fd8147c193239ef31d
Adam Oswalt <email@adamoswalt.com>
2016-11-14 17:54:44 -0600

    lower and upper casing for string

--- pig.clj
+++ pig.clj
@@ -1,14 +1,17 @@
 ;NOTE(adam): a function that translates a text to Pig Latin and back
 
+;NOTE(adam): change each word in a string with the function
 (defn changeWords [string changer]
-  (def stringList (.split string " "))
+  (def stringList (.split (.toLowerCase string) " "))
   (def changedList (map changer stringList))
   (def changedString (reduce #(str %1 " " %2) changedList))
-  changedString)
+  (str (.toUpperCase  (str (first changedString))) (apply str (rest changedString))))
 
+;NOTE(adam): move first character to end and append "ay"
 (defn toPigLatin [string]
   (changeWords string #(str (apply str (rest %1)) (first %1) "ay")))
 
+;NOTE(adam): strip ending "ay" and move last character to beginning
 (defn fromPigLatin [string]
   (changeWords string #(apply str (last (drop-last 2 %1)) (drop-last 3 %1))))
 
