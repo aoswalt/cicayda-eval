d6df2e74dd30f7534bcdf58f8ae6be7a251063d2
Adam Oswalt <email@adamoswalt.com>
2016-11-14 13:27:05 -0600

    frame function in clojure

--- framing.clj
+++ framing.clj
@@ -1,20 +1,32 @@
 ;NOTE(adam): function that takes a list of strings and prints them, one per line, in a rectangular frame.
 
+;NOTE(adam):
 (defn longer [a b]
   (if (> (count a) (count b)) a b))
 
+;NOTE(adam): right pad a string with spaces to be the longestLength
 (defn stringPad [string longestLength]
-  (apply str string (repeat (- longestLength (count string)) "-")))
+  (apply str string (repeat (- longestLength (count string)) " ")))
 
-(defn frame [stringList]
+;NOTE(adam): construct a framed message
+(defn frameStrings [stringList frameChar]
   (def longest (reduce longer stringList))
   (def longestLength (count longest))
-  (def cap (str "**" (apply str (repeat longestLength "*")) "**"))
-  (print cap)
-  (print \newline)
-  (map #(str "* " (stringPad %1 longestLength) " *") stringList))
 
-(print (reduce longer ["different" "strings"]))
-(print \newline)
+  (def cap (str frameChar frameChar (apply str (repeat longestLength frameChar)) frameChar frameChar))
+  (def body (map #(str frameChar " " (stringPad %1 longestLength) " " frameChar) stringList))
+
+  (def bodyCapped (concat [cap] body [cap]))
+  (def bodyLined (map #(str %1 \newline) bodyCapped))
+  (apply str bodyLined))
+
+;NOTE(adam): multi-variadic function to supply optional arg
+(defn frame
+  ([stringList] (frameStrings stringList "*"))
+  ([stringList frameChar] (frameStrings stringList frameChar)))
+
+
 (print (frame ["Hello" "World" "in" "a" "frame"]))
+(print \newline)
+(print (frame ["A" "different" "set" "of" "strings" "to" "test"] "#"))
 (print \newline)
